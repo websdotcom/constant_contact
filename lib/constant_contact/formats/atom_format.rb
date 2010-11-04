da3e0ab15eebcf768e9e285add50e9dc6be7a9f8
idris <idris.m@gmail.com>
2010-11-04 15:44:20 -0400

    Pagination support.  * Now when a collection result has more than 50 results and is paginated by Constant Contact, it automatically pulls ALL pages, rather than just the first.  * This bumps us to version 1.3.0

--- lib/constant_contact/formats/atom_format.rb
+++ lib/constant_contact/formats/atom_format.rb
@@ -18,36 +18,46 @@ module ActiveResource
 
       def decode(xml)
         xml.gsub!( /\<(\/?)atom\:/, '<\1' ) # the "events" feeds have "atom:" in front of tags, for some reason
-        return [] if no_content?(xml)
-        result = Hash.from_xml(from_atom_data(xml))
-        is_collection?(xml) ? result['records'] : result.values.first
+        doc = REXML::Document.new(xml)
+        return [] if no_content?(doc)
+        result = Hash.from_xml(from_atom_data(doc))
+
+        if is_collection?(doc)
+          list = result['records']
+
+          next_link = REXML::XPath.first(doc, "/feed/link[@rel='next']")
+          if next_link
+            next_path = next_link.attribute('href').value
+            next_page = ::ConstantContact::Base.connection.get(next_path)
+            list.concat(next_page)
+          end
+
+          list
+        else
+          result.values.first
+        end
       end
       
 
       private
       
-      def from_atom_data(xml)
-        if is_collection?(xml)
-          result = content_from_collection(xml)
+      def from_atom_data(doc)
+        if is_collection?(doc)
+          content_from_collection(doc)
         else
-          result = content_from_single_record(xml)
+          content_from_single_record(doc)
         end
-        result.delete('xmlns')
-        return result
       end
       
-      def no_content?(xml)
-        doc = REXML::Document.new(xml)
+      def no_content?(doc)
         REXML::XPath.match(doc,'//content').size == 0
       end
       
-      def is_collection?(xml)
-        doc = REXML::Document.new(xml)
+      def is_collection?(doc)
         REXML::XPath.match(doc,'//content').size > 1
       end
 
-      def content_from_single_record(xml)
-        doc = REXML::Document.new(xml)
+      def content_from_single_record(doc)
         str = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
         REXML::XPath.each(doc, '//content') do |e|
           content = e.children[1]
@@ -56,19 +66,17 @@ module ActiveResource
         str
       end
       
-      def content_from_collection(xml)
-        doc = REXML::Document.new(xml)
+      def content_from_collection(doc)
         str = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><records type=\"array\">"
         REXML::XPath.each(doc, '//content') do |e|
           content = e.children[1]
           str << content.to_s
         end
-        str << "</records>" 
+        str << "</records>"
         str
       end
       
-      def content_from_member(xml)
-        doc = REXML::Document.new(xml)
+      def content_from_member(doc)
         str = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
         REXML::XPath.each(doc, '//content') do |e|
          content = e.children[1].children
--- lib/constant_contact/version.rb
+++ lib/constant_contact/version.rb
@@ -1,3 +1,3 @@
 module ConstantContact
-  Version = '1.2.2'.freeze
+  Version = '1.3.0'.freeze
 end
